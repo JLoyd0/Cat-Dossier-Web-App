a7fafce607b3b07192bc0704f2094e2738230118
whiteNight <116985492+JLoyd0@users.noreply.github.com>
2025-12-01 05:53:46 +0800

    Add files via upload

--- CatDossier Web App/index.js
+++ CatDossier Web App/index.js
@@ -10,3 +10,35 @@ function SignUp() {
     LogInForm.style.transform = "translateX(0px)";
     SignUpForm.style.transform = "translateX(0px)";
 }
+
+function handleLogIn() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const remember = document.getElementById('remember').checked;
+    const errorDiv = document.getElementById('loginError'); 
+    
+
+    if(username === "admin" && password === "1234") {
+        if(remember) {
+            localStorage.setItem("savedUsername", username);
+        } else {
+            localStorage.removeItem("savedUsername");
+        }
+        window.location.href = "dashboard.html";
+    } else {
+
+        errorDiv.textContent = "Incorrect username or password!";
+        errorDiv.style.display = "block";
+    }
+
+    return false;
+}
+
+
+window.onload = () => {
+    const savedUsername = localStorage.getItem("savedUsername");
+    if(savedUsername) {
+        document.getElementById('username').value = savedUsername;
+        document.getElementById('remember').checked = true;
+    }
+};
